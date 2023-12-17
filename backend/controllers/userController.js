f9f740419db870581ee5d13f1f61c55543cb520f
Inderjeet Singh <inder12801@gmail.com>
2023-12-18 00:54:32 +0530

    backend added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,108 @@
+// const UserModel = require("../modals/userModel");
+// const expressAsyncHandler = require("express-async-handler");
+import { encryptPassword } from "../config/encryptPassword.js";
+import { generateToken } from "../config/generateToken.js";
+import User from "../models/userModel.js";
+import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcrypt";
+import { emailValidator } from "../utils/emailValidator.js";
+
+// Login
+const loginController = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json("Invalid credentials");
+    } else {
+      // compare passwords
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        return res.status(401).send("Invalid Password");
+      } else {
+        res.status(201);
+        res.json({
+          name: user.name,
+          email: user.email,
+          pic: user.pic,
+          _id: user._id,
+          token: generateToken(user._id),
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Registration
+const registerController = expressAsyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  try {
+    if (!name || !email || !password) {
+      res.send(400);
+      throw Error("All necessary input fields have not been filled");
+    }
+    // write a email verification using regex
+    if (!emailValidator(email)) {
+      res.send(400);
+      throw Error("Invalid Email");
+    }
+
+    // pre-existing user
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      // res.send(405);
+      throw new Error("User already Exists");
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create an entry in the db
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      pic,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        pic: user.pic.toString(),
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("Registration Error");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+
+  // check for all fields
+});
+
+const allUsers = expressAsyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+
+  const users = await User.find(keyword)
+    .find({
+      _id: { $ne: req.user._id },
+    })
+    .select("-password");
+  res.send(users);
+});
+
+export { loginController, registerController, allUsers };
---
+++ backend/index.js
@@ -0,0 +1,88 @@
+import express from "express";
+import path from "path";
+import { Server as SocketIOServer, Socket } from "socket.io";
+import { chats } from "./data/dummyData.js";
+import dotenv from "dotenv";
+import { connectToDB } from "./config/connectToDB.js";
+import userRoute from "./routes/userRoute.js";
+import chatRoute from "./routes/chatRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+
+// creating a server
+const app = express();
+
+// middlewares
+app.use(express.json());
+dotenv.config();
+
+// connect to database
+connectToDB();
+
+app.get("/", (req, res) => {
+  res.send("server running succesfully!!");
+});
+
+app.use("/api/user", userRoute);
+
+app.use("/api/chat", chatRoute);
+
+app.use("/api/message", messageRoute);
+
+app.get("/api/chat/:id", (req, res) => {
+  const chat = chats.find((chat) => chat._id === req.params.id);
+  if (!chat) return res.status(404).send({ message: "Chat not found" });
+  res.json(chat);
+});
+
+const server = app.listen(5000, () => {
+  console.log("Server is running on port : ", 5000);
+});
+
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+  pingInterval: 60000,
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle chat events
+  socket.on("setup", (userData) => {
+    // Join the chat room
+    socket.join(userData?._id);
+    console.log(userData?._id);
+    socket.emit("connected");
+  });
+  socket.on("join-chat", (room) => {
+    socket.join(room);
+    console.log(`A user joined chat: ${room}`);
+  });
+  socket.on("new-message", (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("Chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message-received", newMessageRecieved);
+    });
+  });
+
+  // Handle typing event
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing", room);
+  });
+  // Handle stop typing event
+  socket.on("stop-typing", (room) => {
+    socket.in(room).emit("stop-typing", room);
+  });
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Export the server and io instance
+export { server, io };
