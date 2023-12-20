e80961ae0b3ff2aaf0ce521b22ef1e84180d84b5
Inderjeet Singh <inder12801@gmail.com>
2023-12-20 20:12:38 +0530

    update : ChatProvider

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -1,6 +1,7 @@
-import { React, createContext, useContext, useEffect, useState } from "react";
+import * as React from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-export const API_URL = import.meta.env.VITE_SERVER_API_URL;
+// export const API_URL = import.meta.env.VITE_SERVER_API_URL;
 
 const ChatContext = createContext();
 const ChatProvider = ({ children }) => {
